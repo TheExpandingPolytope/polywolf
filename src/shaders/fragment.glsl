1257b45744b157a7da48400560b3bf1a76c46761
Jesse Engerman <jesseengerman@hotmail.com>
2019-06-04 20:55:26 -0500

    pbr initialization

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,8 +1,10 @@
 #version 300 es
+#define NUM_LIGHTS 1
 precision mediump float;
 
-in vec3 normal;
-in vec2 tex_coords;
+in vec3 v_position;
+in vec3 v_normal;
+in vec2 v_texcoords;
 
 uniform sampler2D emissive_texture;
 uniform sampler2D normal_texture;
@@ -10,9 +12,35 @@ uniform sampler2D occlusion_texture;
 uniform sampler2D base_color_texture;
 uniform sampler2D metallic_roughness_texture;
 
+//material variables
+vec4 base_color;
+vec3 normal;
+float metallic;
+float roughness;
+float occlusion;
+vec3 emissive;
+
+//light variables
+vec3 light_positions[NUM_LIGHTS];
+vec3 light_colors[NUM_LIGHTS];
+
 out vec4 color;
+
+//cook torrance
+
  
 void main() {
-  color = texture(emissive_texture, tex_coords)+
-          texture(base_color_texture, tex_coords);
+  //set material info
+  base_color = texture(base_color_texture, v_texcoords);
+  normal = normalize(v_normal + texture(normal_texture, v_texcoords).xyz);
+  metallic = texture(metallic_roughness_texture, v_texcoords).b;
+  roughness = texture(metallic_roughness_texture, v_texcoords).g;
+  occlusion = texture(occlusion_texture, v_texcoords).r;
+  emissive = texture(emissive_texture, v_texcoords).rgb;
+
+  //set geometry info
+
+  //set color
+  color = base_color;
+
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,17 +1,19 @@
 #version 300 es
 
 layout( location = 0 ) in vec3 position;
-layout( location = 1 ) in vec3 norm;
-layout( location = 2 ) in vec2 tex;
+layout( location = 1 ) in vec3 normal;
+layout( location = 2 ) in vec2 texcoords;
 
 uniform mat4 perspective;
 uniform mat4 view;
 
-out vec3 normal;
-out vec2 tex_coords;
+out vec3 v_position;
+out vec3 v_normal;
+out vec2 v_texcoords;
 
 void main(){
     gl_Position = perspective*view*vec4(position, 1.0);
-    normal = norm;
-    tex_coords = tex;
+    v_position = (view*vec4(position, 1.0)).xyz;
+    v_normal = normal;
+    v_texcoords = texcoords;
 }
