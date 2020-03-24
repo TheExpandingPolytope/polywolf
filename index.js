17661fb02d00604c3961f69685f0ff2e70e81069
Jesse Engerman <jesseengerman@hotmail.com>
2020-03-24 09:12:25 -0500

    layout 1.0.4

--- index.js
+++ index.js
@@ -79,11 +79,9 @@ class PolyWolf extends HTMLDivElement {
             padding:0;
             margin:0;
             border-width:0.5px;
-            border-radius:10px;
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-            background-color: #F5F7FF;
-            background: radial-gradient(#F5F7FF,#F5FFFF);
-            cursor:cell;
+            box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19);
+            background-color: #55555;
+            cursor:grab;
         `;
 
         //add canvas
@@ -115,11 +113,11 @@ class PolyWolf extends HTMLDivElement {
         this.button.innerHTML = `
         <svg version="1.1" id="play" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" height="50px" width="50px"
 	 viewBox="0 0 100 100" enable-background="new 0 0 100 100" xml:space="preserve">
-  <path class="stroke-solid" fill="none" stroke="white"  d="M49.9,2.5C23.6,2.8,2.1,24.4,2.5,50.4C2.9,76.5,24.7,98,50.3,97.5c26.4-0.6,47.4-21.8,47.2-47.7
+  <path class="stroke-solid" fill="none" stroke="rgba(0,0,0,.4)"  d="M49.9,2.5C23.6,2.8,2.1,24.4,2.5,50.4C2.9,76.5,24.7,98,50.3,97.5c26.4-0.6,47.4-21.8,47.2-47.7
     C97.3,23.7,75.7,2.3,49.9,2.5"/>
-  <path class="stroke-dotted" fill="none" stroke="white"  d="M49.9,2.5C23.6,2.8,2.1,24.4,2.5,50.4C2.9,76.5,24.7,98,50.3,97.5c26.4-0.6,47.4-21.8,47.2-47.7
+  <path class="stroke-dotted" fill="none" stroke="rgba(0,0,0,.4)"  d="M49.9,2.5C23.6,2.8,2.1,24.4,2.5,50.4C2.9,76.5,24.7,98,50.3,97.5c26.4-0.6,47.4-21.8,47.2-47.7
     C97.3,23.7,75.7,2.3,49.9,2.5"/>
-  <path class="icon" fill="white" d="M38,69c-1,0.5-1.8,0-1.8-1.1V32.1c0-1.1,0.8-1.6,1.8-1.1l34,18c1,0.5,1,1.4,0,1.9L38,69z"/>
+  <path class="icon" fill="rgba(0,0,0,.4)" d="M38,69c-1,0.5-1.8,0-1.8-1.1V32.1c0-1.1,0.8-1.6,1.8-1.1l34,18c1,0.5,1,1.4,0,1.9L38,69z"/>
 </svg>
         `
         this.button.style = `
